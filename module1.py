d2205965403474d8cfbd099f3218c4b5bb4282bb
0xvasq <jorge.vasquez007@mymdc.net>
2024-06-16 20:10:35 -0400

    linkedlist practice

--- module1.py
+++ module1.py
@@ -1,4 +1,72 @@
 from queue import PriorityQueue
+from signal import valid_signals
+
+
+
+'''
+class Block:
+    def __init__(self, value, localTop):
+        self.value = value
+        self.localTop = localTop
+
+class Stack:
+    def __init__(self, size):
+        self.stack = [None] * size
+        self.size = size
+        self.top = -1
+
+    def push(self, value):
+        if self.top == self.size - 1:
+            print("Stack is full")
+        else:
+            self.top += 1
+            if self.top == 0:
+                self.stack[self.top] = Block(value, value)
+            else:
+                if self.stack[self.top-1].localMax > value:
+                    self.stack[self.top] = Block(value, self.stack[self.top - 1].localMax)
+                else:
+                    self.stack
+                    self.stack[self.top] = Block(value, value)
+        print(value, "added to the stack")
+
+    def pop(self):
+        if self.stack[self.top] == -1:
+            print("stack is full")
+        else:
+            self.top -= 1
+
+    def max(self):
+        if self.stack[self.top] == -1:
+            print("stakc is full")
+        else: 
+            print("max is: ", self.stack[self.top].localMax)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 '''
 class Block:
@@ -157,10 +225,10 @@ def printKMostFreq(file_path, k):
 
 '''
 
-
+'''
 class Solution(object):
     def mergeTwoList(self, list1, list2):
-        dummyNode = tail = LinkNode()
+        dummyNode = tail = ListNode()
 
         while list1 and list2:
             if list1.val < list2.val:
@@ -178,4 +246,105 @@ class Solution(object):
             tail.next = list2
 
         return dummyNode
+
+'''
+
+'''
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val 
+        self.next = next
+
+def addTwoNums(l1, l2):
+    tempNode = ListNode()
+    now = tempNode
+    sum = 0
+ 
+
+    while l1 or l2 or sum:
+        if l1:
+           num1 = l1.val
+           l1 = l1.next
+        else:
+            num1 = 0
+
+        if l2:
+            num2 = l2.val
+            l2 = l2.next
+        else:
+            num2 = 0
+
+        sum_Total = num1 + num2 + sum
+        sum = sum_Total // 10
+        num = sum_Total % 10
+
+        now.next = ListNode(num)
+        now = now.next
+
+        if sum > 0:
+            now.next = ListNode(sum)
+
+    return tempNode.next
+
+
+
+
+if __name__ == "__main__":
+    l1 = ListNode(2, ListNode(4, ListNode(3)))
+    l2 = ListNode(5, ListNode(6, ListNode(4)))
+
+    result = addTwoNums(l1, l2)
+
+    while result:
+        print(result.val, end=" ")
+        result = result.next
+
+
+'''
+
+'''
+
+class ListNode:
+    def __init__(self, val=0,next=none):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        temp = ListNode(0, head)
+        left = temp
+        curr = head
+
+        while n > 0 and curr:
+            curr = curr.next
+            n-=1
+        while curr:
+            left = left.next
+            curr = curr.next
+
+        left.next = left.next.next
+        return temp.next
+
+def create_linked_list(arr):
+    if not arr:
+        return None
+    
+    head = ListNode(arr[0])
+    current = head
+    for val in arr[1:]:
+        current.next = ListNode(val)
+
+'''
+
+def middleNode(head):
+    if head is None:
+        return None
+    slow = head
+    fast = head
+
+    while fast is not None and fast.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+
+    return slow
 
